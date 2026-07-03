eb5e5a17f16e332b4d67854f463f503abc7ff819
SevenOverBravo <naferb9@gmail.com>
2026-07-03 04:08:25 -0400

    Update documentation.sql

--- documentation.sql
+++ documentation.sql
@@ -1,15 +1,18 @@
 -- BUILDING THE SCHEMA --
 
+-- Create Schema --
+CREATE SCHEMA `olist` ;
+
 -- Orders Table --
 CREATE TABLE `olist`.`orders` (
   `order_id` VARCHAR(32) NOT NULL,
   `customer_id` VARCHAR(32) NULL,
   `order_status` VARCHAR(45) NULL,
-  `order_purchase_timestamp` TIMESTAMP NULL,
-  `order_approved_at` TIMESTAMP NULL,
-  `order_delivered_carrier_date` TIMESTAMP NULL DEFAULT NULL,
-  `order_delivery_customer_date` TIMESTAMP NULL DEFAULT NULL,
-  `order_estimated_delivery_date` TIMESTAMP NULL DEFAULT NULL,
+  `order_purchase_timestamp` DATETIME NULL,
+  `order_approved_at` DATETIME NULL,
+  `order_delivered_carrier_date` DATETIME NULL DEFAULT NULL,
+  `order_delivery_customer_date` DATETIME NULL DEFAULT NULL,
+  `order_estimated_delivery_date` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`order_id`));
 
 -- Load Orders CSV Into Table --
