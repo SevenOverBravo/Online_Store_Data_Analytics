73b314d5d7f88d7b889f042a878b6899fc02403a
SevenOverBravo <naferb9@gmail.com>
2026-07-09 03:05:44 -0400

    Update documentation.sql

--- documentation.sql
+++ documentation.sql
@@ -129,3 +129,78 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+-- BASELINE INFO --
+
+-- Number of customers from each state --
+SELECT customer_state, COUNT(customer_id) AS NUM_CUSTOMERS
+FROM customers
+GROUP BY customer_state
+ORDER BY NUM_CUSTOMERS DESC
+  -- Top 3 are SP (41746), RJ (12852), and MG (11635) out of 112650 customers
+  
+-- Average # of items purchased per order --
+SELECT AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM orders o JOIN items i 
+	    ON o.order_id = i.order_id
+	  GROUP BY o.order_id) AS INF 
+    -- Returns 1.142, but neglects any possible outliers in cities with low customer counts
+
+-- Recalculate Average with Caveat: Remove orders associated with customers from cities with less than 20 customers overall
+WITH elligible_cities (customer_city) 
+AS (
+	SELECT customer_city
+    FROM customers
+    GROUP BY customer_city
+    HAVING COUNT(DISTINCT customer_id)>=20)
+SELECT AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT c.customer_city, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM customers c
+		JOIN orders o ON c.customer_id=o.customer_id
+        JOIN items i ON o.order_id=i.order_id
+      WHERE c.customer_city IN (SELECT * FROM elligible_cities)
+	  GROUP BY c.customer_city, o.order_id) AS DIST
+   -- Returns 1.144; Not significantly different from non-filtered counterpart
+  
+-- Analysis Pt. 1: Customer Factors (Location) --
+
+-- First: Confirm No Null/Improper Entries in Target Columns --
+SELECT COUNT(zip_code_prefix) AS ZIP_NUM, COUNT(customer_city) AS CITY_NUM, COUNT(customer_state) AS STATE_NUM
+FROM customers
+WHERE (zip_code_prefix IS NOT NULL AND zip_code_prefix REGEXP '^[0-9]{4,5}$') -- Not Null and a sequence of 4 or 5 numbers
+	AND customer_city IS NOT NULL
+  AND (customer_state IS NOT NULL AND customer_state REGEXP '^[A-Z]{2}$'); -- Not Null and a sequence of 2 letters
+  -- Output shows no row is invalid (Counts = Number of Rows)
+
+-- Customer State: Average items ordered and Number of customers for each
+WITH elligible_cities (customer_city) 
+AS (
+	SELECT customer_city
+    FROM customers
+    GROUP BY customer_city
+    HAVING COUNT(DISTINCT customer_id)>=20)
+SELECT DIST.customer_state, COUNT(DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT c.customer_state, c.customer_city, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM customers c
+		JOIN orders o ON c.customer_id=o.customer_id
+        JOIN items i ON o.order_id=i.order_id
+      WHERE c.customer_city IN (SELECT * FROM elligible_cities)
+	  GROUP BY c.customer_state, c.customer_city, o.order_id) AS DIST
+GROUP BY DIST.customer_state
+ORDER BY AVG_ITEMS_PER_ORDER DESC
+  -- Output: Ranges from 
+  
+-- Customer City: Average items ordered and Number of customers for each 
+SELECT DIST.customer_city, COUNT(DIST.customer_id) AS NUM_CUSTOMERS, AVG(DIST.order_item_id) as AVG_ITEMS
+FROM (SELECT DISTINCT c.customer_city, c.customer_id, i.order_item_id
+FROM customers c, orders o, items i
+WHERE o.customer_id = c.customer_id
+	AND o.order_id = i.order_id) AS DIST
+GROUP BY DIST.customer_city
+ORDER BY AVG_ITEMS DESC
+ -- Many cities with 4, 5, or up to 6.5 items purchased per order on average
+ -- Majority of cities in top 20 (for average items purchased per order) a
+
+-- Additional Query: State, City, and Zip Code -- 
+
