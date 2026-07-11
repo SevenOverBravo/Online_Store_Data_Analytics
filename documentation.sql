6041824a4265f19c0e707133ccbceae36fa5d866
SevenOverBravo <naferb9@gmail.com>
2026-07-11 00:40:02 -0400

    Update documentation.sql

--- documentation.sql
+++ documentation.sql
@@ -130,6 +130,8 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
+-- Analysis Pt. 1: Customer Factors (Location) --
+
 -- BASELINE INFO --
 
 -- Number of customers from each state --
@@ -144,7 +146,7 @@ SELECT AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
 FROM (SELECT o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
 	  FROM orders o JOIN items i 
 	    ON o.order_id = i.order_id
-	  GROUP BY o.order_id) AS INF 
+	  GROUP BY o.order_id) AS DIST
     -- Returns 1.142, but neglects any possible outliers in cities with low customer counts
 
 -- Recalculate Average with Caveat: Remove orders associated with customers from cities with less than 20 customers overall
@@ -162,8 +164,6 @@ FROM (SELECT c.customer_city, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
       WHERE c.customer_city IN (SELECT * FROM elligible_cities)
 	  GROUP BY c.customer_city, o.order_id) AS DIST
    -- Returns 1.144; Not significantly different from non-filtered counterpart
-  
--- Analysis Pt. 1: Customer Factors (Location) --
 
 -- First: Confirm No Null/Improper Entries in Target Columns --
 SELECT COUNT(zip_code_prefix) AS ZIP_NUM, COUNT(customer_city) AS CITY_NUM, COUNT(customer_state) AS STATE_NUM
@@ -174,33 +174,177 @@ WHERE (zip_code_prefix IS NOT NULL AND zip_code_prefix REGEXP '^[0-9]{4,5}$') --
   -- Output shows no row is invalid (Counts = Number of Rows)
 
 -- Customer State: Average items ordered and Number of customers for each
-WITH elligible_cities (customer_city) 
+WITH elligible_cities (customer_state, customer_city) 
 AS (
-	SELECT customer_city
+	SELECT customer_state, customer_city
     FROM customers
-    GROUP BY customer_city
+    GROUP BY customer_state, customer_city
     HAVING COUNT(DISTINCT customer_id)>=20)
 SELECT DIST.customer_state, COUNT(DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
 FROM (SELECT c.customer_state, c.customer_city, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
 	  FROM customers c
 		JOIN orders o ON c.customer_id=o.customer_id
         JOIN items i ON o.order_id=i.order_id
-      WHERE c.customer_city IN (SELECT * FROM elligible_cities)
+      WHERE (c.customer_state, c.customer_city) IN (SELECT customer_state, customer_city FROM elligible_cities)
 	  GROUP BY c.customer_state, c.customer_city, o.order_id) AS DIST
 GROUP BY DIST.customer_state
-ORDER BY AVG_ITEMS_PER_ORDER DESC
-  -- Output: Ranges from 
+  -- Output: Top 3 states in terms of average items per order (AIPO) are MT (1.193), GO (1.180), and PB (1.165); not much different from overall average of 1.144
+  -- Eight of the ten states with above-average AIPO have >300 orders; high sample size indicates accurate averages
   
--- Customer City: Average items ordered and Number of customers for each 
-SELECT DIST.customer_city, COUNT(DIST.customer_id) AS NUM_CUSTOMERS, AVG(DIST.order_item_id) as AVG_ITEMS
-FROM (SELECT DISTINCT c.customer_city, c.customer_id, i.order_item_id
-FROM customers c, orders o, items i
-WHERE o.customer_id = c.customer_id
-	AND o.order_id = i.order_id) AS DIST
-GROUP BY DIST.customer_city
-ORDER BY AVG_ITEMS DESC
- -- Many cities with 4, 5, or up to 6.5 items purchased per order on average
- -- Majority of cities in top 20 (for average items purchased per order) a
+-- Customer City: Average items ordered and Number of customers for each --
+WITH elligible_cities (customer_state, customer_city) 
+AS (
+	SELECT customer_state, customer_city
+    FROM customers
+    GROUP BY customer_state, customer_city
+    HAVING COUNT(DISTINCT customer_id)>=20)
+SELECT DIST.customer_state, DIST.customer_city, COUNT(DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT c.customer_state, c.customer_city, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM customers c
+		JOIN orders o ON c.customer_id=o.customer_id
+        JOIN items i ON o.order_id=i.order_id
+      WHERE (c.customer_state, c.customer_city) IN (SELECT customer_state, customer_city FROM elligible_cities)
+	  GROUP BY c.customer_state, c.customer_city, o.order_id) AS DIST
+GROUP BY DIST.customer_state, DIST.customer_city
+ORDER BY AVG_ITEMS_PER_ORDER DESC
+ -- Top 3 cities are Uniao da Vitoria (PR, AIPO = 1.621), Peruibe (SP, AIPO = 1.455), and Santana do Livramento (RS, AIPO = 1.450)
+ -- In the Top 20 cities, eight are in SP, three in SC, two in RS, PR, and RJ, and one in MT, GO, and BA
+ -- Accuracy of averages is less certain due to most sample sizes falling between 20 and 100
 
--- Additional Query: State, City, and Zip Code -- 
+-- Customer Zip Code: Average items ordered and Number of customers for each --
+WITH elligible_cities (customer_state, customer_city) 
+AS (
+	SELECT customer_state, customer_city
+    FROM customers
+    GROUP BY customer_state, customer_city
+    HAVING COUNT(DISTINCT customer_id)>=20)
+SELECT DIST.customer_state, DIST.customer_city, DIST.zip_code_prefix, COUNT(DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT c.customer_state, c.customer_city, c.zip_code_prefix, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM customers c
+		JOIN orders o ON c.customer_id=o.customer_id
+        JOIN items i ON o.order_id=i.order_id
+      WHERE (c.customer_state, c.customer_city) IN (SELECT customer_state, customer_city FROM elligible_cities)
+	  GROUP BY c.customer_state, c.customer_city, c.zip_code_prefix, o.order_id) AS DIST
+GROUP BY DIST.customer_state, DIST.customer_city, DIST.zip_code_prefix
+ORDER BY AVG_ITEMS_PER_ORDER DESC
+ -- Multiple zip codes in the ranges of 4-7 AIPO; primarily located in SP
+ -- Number of orders is so low that this is unikely to extrapolate to a larger order sample
 
+-- Analysis Part 2: Seller Factors --
+
+-- Seller ID (note that sellers are left unnamed in the data set and are only identified by unique IDs)--
+
+-- Begin with basic query displaying unfiltered AIPO and number of orders for each seller ID --
+SELECT DIST.seller_id, COUNT(DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT s.seller_id, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM items i
+		  JOIN orders o ON i.order_id=o.order_id
+		  JOIN sellers s ON s.seller_id=i.seller_id
+	  GROUP BY s.seller_id, order_id) AS DIST
+GROUP BY DIST.seller_id
+ORDER BY AVG_ITEMS_PER_ORDER DESC
+ -- Output reveals many sellers are only involved with one or two orders; generates possible bias in the average
+
+-- New query, now requiring each seller to be associated with at least 10 orders to be included in AIPO calculation
+WITH elligible_sellers (seller_id) AS (
+	SELECT s.seller_id
+    FROM items i
+		JOIN orders o ON i.order_id=o.order_id 
+        JOIN sellers s ON i.seller_id=s.seller_id
+	GROUP BY s.seller_id
+    HAVING COUNT(DISTINCT o.order_id)>=10) 
+SELECT AVG(NUM_ITEMS) AS AVG_ITEMS_PER_CUSTOMER
+FROM (SELECT s.seller_id, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM items i
+		  JOIN orders o ON i.order_id=o.order_id
+		  JOIN sellers s ON s.seller_id=i.seller_id
+	  WHERE s.seller_id IN (SELECT seller_id FROM elligible_sellers)
+	  GROUP BY s.seller_id, order_id) AS DIST 
+ -- Outputs 1.123, slightly lower than base AIPO of 1.142
+
+-- Rework above query to include seller ID and number of orders 
+WITH elligible_sellers (seller_id) AS (
+	SELECT s.seller_id
+    FROM items i
+		JOIN orders o ON i.order_id=o.order_id 
+        JOIN sellers s ON i.seller_id=s.seller_id
+	GROUP BY s.seller_id
+    HAVING COUNT(DISTINCT o.order_id)>=10) 
+SELECT DIST.seller_id, COUNT(DISTINCT DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_CUSTOMER
+FROM (SELECT s.seller_id, o.order_id, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM items i
+		  JOIN orders o ON i.order_id=o.order_id
+		  JOIN sellers s ON s.seller_id=i.seller_id
+	  WHERE s.seller_id IN (SELECT seller_id FROM elligible_sellers)
+	  GROUP BY s.seller_id, order_id) AS DIST
+GROUP BY DIST.seller_id
+ORDER BY AVG_ITEMS_PER_CUSTOMER DESC
+-- Top three sellers in terms of AIPO are those with IDs beginning in eed78 (2.546), 334ca (2.367), and e7d5b (2.250)
+-- Many sellers with >100 orders also have high AIPOs, like 25c5c (158 orders, AIPO = 1.703) and 1025f (915 orders, AIPO = 1.561)
+-- High order number and AIPO signals accurate long-term averages and opportunities to improve overall AIPO
+
+-- Seller Location --
+
+-- Seller State --
+-- AIPO per seller_state and number of sellers/orders from each, keeping the same filter as above
+WITH elligible_sellers (seller_id) AS (
+	SELECT s.seller_id
+    FROM items i
+		JOIN orders o ON i.order_id=o.order_id 
+        JOIN sellers s ON i.seller_id=s.seller_id
+	GROUP BY s.seller_id
+    HAVING COUNT(DISTINCT o.order_id)>=10) 
+SELECT DIST.seller_state, COUNT(DISTINCT DIST.seller_id) AS NUM_SELLERS, COUNT(DISTINCT DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT s.seller_id, o.order_id, s.seller_state, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM items i
+		  JOIN orders o ON i.order_id=o.order_id
+		  JOIN sellers s ON s.seller_id=i.seller_id
+	  WHERE s.seller_id IN (SELECT seller_id FROM elligible_sellers)
+	  GROUP BY s.seller_id, order_id, s.seller_state) AS DIST
+GROUP BY DIST.seller_state
+ORDER BY AVG_ITEMS_PER_ORDER DESC
+ -- Top three states in terms of AIPO are ES (1.183), BA (1.135), and SP (1.129)
+ -- Increases from base AIPO are minimal, and both ES and BA have low seller/order counts; suggests innacurate averages in the long term
+
+-- Seller City --
+-- AIPO per seller_city (including seller_state) and number of sellers/orders from each
+WITH elligible_sellers (seller_id) AS (
+	SELECT s.seller_id
+    FROM items i
+		JOIN orders o ON i.order_id=o.order_id 
+        JOIN sellers s ON i.seller_id=s.seller_id
+	GROUP BY s.seller_id
+    HAVING COUNT(DISTINCT o.order_id)>=10) 
+SELECT DIST.seller_state, DIST.seller_city, COUNT(DISTINCT DIST.seller_id) AS NUM_SELLERS, COUNT(DISTINCT DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT s.seller_id, o.order_id, s.seller_state, s.seller_city, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM items i
+		  JOIN orders o ON i.order_id=o.order_id
+		  JOIN sellers s ON s.seller_id=i.seller_id
+	  WHERE s.seller_id IN (SELECT seller_id FROM elligible_sellers)
+	  GROUP BY s.seller_id, order_id, s.seller_state) AS DIST
+GROUP BY DIST.seller_state, DIST.seller_city
+ORDER BY AVG_ITEMS_PER_ORDER DESC 
+ -- Top three cities in terms of AIPO are Fernandopolis (state is SP, AIPO = 2.057), Campina das Missoes (state is RS, AIPO = 1.909) and Portao (state is RS, AIPO = 1.8667)
+ -- Most cities have only one seller, but some have high seller, order, and AIPO counts (Sao Jose do Rio Preto, for example) reinforcing the validity of their measurements
+ -- Possible correlation between seller_city and AIPO with some locations, but not robust enough in most cases 
+
+-- Seller Zip Code Prefix --
+-- AIPO per zip_code_prefix (including seller_state and seller_city) and number of sellers/orders from each
+WITH elligible_sellers (seller_id) AS (
+	SELECT s.seller_id
+    FROM items i
+		JOIN orders o ON i.order_id=o.order_id 
+        JOIN sellers s ON i.seller_id=s.seller_id
+	GROUP BY s.seller_id
+    HAVING COUNT(DISTINCT o.order_id)>=10) 
+SELECT DIST.seller_state, DIST.seller_city, DIST.zip_code_prefix, COUNT(DISTINCT DIST.seller_id) AS NUM_SELLERS, COUNT(DISTINCT DIST.order_id) AS NUM_ORDERS, AVG(NUM_ITEMS) AS AVG_ITEMS_PER_ORDER
+FROM (SELECT s.seller_id, o.order_id, s.seller_state, s.seller_city, s.zip_code_prefix, COUNT(i.order_item_id) AS NUM_ITEMS
+	  FROM items i
+		  JOIN orders o ON i.order_id=o.order_id
+		  JOIN sellers s ON s.seller_id=i.seller_id
+	  WHERE s.seller_id IN (SELECT seller_id FROM elligible_sellers)
+	  GROUP BY s.seller_id, order_id, s.seller_state) AS DIST
+GROUP BY DIST.seller_state, DIST.seller_city, DIST.zip_code_prefix
+ORDER BY AVG_ITEMS_PER_ORDER DESC 
+-- Majority of top ten zipcodes are located in SP, with many exceeding an averages items per order of two
+-- Most zipcodes only have one seller; likely high colinearlity with the seller_id findings
